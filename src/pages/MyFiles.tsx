aa8c4949a2c52939fc00fd464c47c8e22ce92f50
gixi <gixi.tsx@gmail.com>
2024-10-23 20:49:22 -0300

    icono de folder

--- src/pages/MyFiles.tsx
+++ src/pages/MyFiles.tsx
@@ -1,5 +1,6 @@
 import { invoke } from "@tauri-apps/api";
 import { documentDir, join } from "@tauri-apps/api/path";
+import { Folder } from "lucide-react";
 import MainContainer from "../components/MainContainer";
 import { MAIN_FOLDER } from "../utils/consts";
 import translations from "../utils/dictionary";
@@ -25,8 +26,9 @@ function MyFiles(): Component {
       </p>
       <button
         onClick={openFolder}
-        className="bg-indigo-600 hover:bg-indigo-500 duration-100 px-6 py-2 text-xl sm:text-2xl font-medium font-serif rounded-lg border border-indigo-400">
-        {d.OpenFolder}
+        className="bg-indigo-600 hover:bg-indigo-500 duration-100 px-6 py-2 text-xl sm:text-2xl font-medium font-serif rounded-lg border border-indigo-400 flex justify-center items-center gap-x-4">
+        <Folder size={24} color="white" />
+        <span>{d.OpenFolder}</span>
       </button>
     </MainContainer>
   );
